9098e0e8995cdb6de4cf1184b08b0381c2be940e
cgutierrezgarcia <cgg18011993@gmail.com>
2022-02-05 12:58:58 +0100

    Parte 4

--- database/migrations/2022_02_05_104504_create_products_table.php
+++ database/migrations/2022_02_05_104504_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -28,6 +29,8 @@ class CreateProductsTable extends Migration
             $table->foreignId('brand_id')->references('id')->on('brands');
 
             $table->integer('quantity')->nullable();
+
+            $table->enum('status', [Product::BORRADOR, Product::PUBLICADO])->default(Product::BORRADOR);
 
             $table->timestamps();
         });
