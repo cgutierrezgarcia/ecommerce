556bb2205fde9b33f6cfb0f6cdf7fdae681c98ec
cgutierrezgarcia <cgg18011993@gmail.com>
2022-02-06 20:03:07 +0100

    Parte 12

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class AddCartItem extends Component
@@ -9,10 +11,12 @@ class AddCartItem extends Component
     public $product;
     public $quantity;
     public $qty = 1;
+    public $options = [];
 
     public function mount()
     {
         $this->quantity = $this->product->quantity;
+        $this->options['image'] = Storage::url($this->product->images->first()->url);
     }
 
     public function decrement()
@@ -27,5 +31,19 @@ class AddCartItem extends Component
     public function render()
     {
         return view('livewire.add-cart-item');
+    }
+
+    public function addItem()
+    {
+        Cart::add([
+            'id' => $this->product->id,
+            'name' => $this->product->name,
+            'qty' => $this->qty,
+            'price' => $this->product->price,
+            'weight' => 550,
+            'options' => $this->options,
+        ]);
+
+        $this->emitTo('dropdown-cart', 'render');
     }
 }
