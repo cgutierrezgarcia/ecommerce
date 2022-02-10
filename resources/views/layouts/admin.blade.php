c6ccd794cf8310131aa6037f6ad640883059b5f6
cgutierrezgarcia <cgg18011993@gmail.com>
2022-02-10 11:10:36 +0100

    Parte 25 - Fin semana 4

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/sweetalert2/dist/sweetalert2.css') }}">
+        <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 
         @livewireStyles
 
@@ -21,6 +22,7 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
         <script src="{{ asset('vendor/ckeditor5/build/ckeditor.js') }}"></script>
         <script src="{{ asset('vendor/sweetalert2/dist/sweetalert2.js') }}"></script>
+        <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
