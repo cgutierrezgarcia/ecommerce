8f35996c809a4f2867d14c9fe703b3e840c02798
cgutierrezgarcia <cgg18011993@gmail.com>
2022-02-10 13:48:42 +0100

    Parte 27

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -13,10 +13,21 @@ class CategoryFilter extends Component
 
     public $category, $subcategoria, $marca;
     public $view = 'grid';
+    public $queryString = ['subcategoria', 'marca'];
+
+    public function updatedSubcategoria()
+    {
+        $this->resetPage();
+    }
+
+    public function updatedMarca()
+    {
+        $this->resetPage();
+    }
 
     public function limpiar()
     {
-        $this->reset(['subcategoria', 'marca']);
+        $this->reset(['subcategoria', 'marca', 'page']);
     }
 
     public function render()
@@ -27,7 +38,7 @@ class CategoryFilter extends Component
 
         if ($this->subcategoria) {
             $productsQuery = $productsQuery->whereHas('subcategory', function(Builder $query){
-                $query->where('name', $this->subcategoria);
+                $query->where('slug', $this->subcategoria);
             });
         }
 
