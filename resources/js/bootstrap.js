d995112aaf1c08c446c26217c1e839a823333a4a
cgutierrezgarcia <cgg18011993@gmail.com>
2022-02-27 18:23:24 +0100

    Tarea 1: instalación e importación de librerías

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+import { Glider } from "glider-js";
+
+import { flexslider } from "flexslider";
